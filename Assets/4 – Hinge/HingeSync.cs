2d1f23fc12f43dd92d877fb38e4d11f190018033
Nir Lahad <nirlah@gmail.com>
2020-01-17 15:49:54 +0200

    Part four – gate hinge

---
+++ Assets/4 – Hinge/HingeSync.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class HingeSync : MonoBehaviour
+{
+  #region Connstants
+
+  private static readonly int ANGLE = Shader.PropertyToID("_Angle");
+
+  #endregion
+
+
+  #region Inspector
+
+  [SerializeField]
+  private HingeJoint hinge = null;
+
+  [SerializeField]
+  private SpriteRenderer spriteRenderer = null;
+
+  #endregion
+
+
+  #region MonoBehaviour
+
+  private void Update ()
+  {
+    float angle = -hinge.angle;
+    spriteRenderer.material.SetFloat(ANGLE, angle);
+  }
+
+  #endregion
+}
