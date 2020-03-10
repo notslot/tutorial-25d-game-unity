73e7b80ec4c64cc0036315d8016d5fc6e9c61dcd
Nir Lahad <nirlah@gmail.com>
2020-03-10 16:20:24 +0200

    Part Five – Cinemachine

---
+++ Assets/5 – Camera/CamZone.cs
@@ -0,0 +1,40 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class CamZone : MonoBehaviour
+{
+  #region Inspector
+
+  [SerializeField]
+  private CinemachineVirtualCamera virtualCamera = null;
+
+  #endregion
+
+
+  #region MonoBehaviour
+
+  private void Start ()
+  {
+    virtualCamera.enabled = false;
+  }
+
+  private void OnTriggerEnter (Collider other)
+  {
+    if ( other.CompareTag("Player") )
+      virtualCamera.enabled = true;
+  }
+
+  private void OnTriggerExit (Collider other)
+  {
+    if ( other.CompareTag("Player") )
+      virtualCamera.enabled = false;
+  }
+
+  private void OnValidate ()
+  {
+    GetComponent<Collider>().isTrigger = true;
+  }
+
+  #endregion
+}
